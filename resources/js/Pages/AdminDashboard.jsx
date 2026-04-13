00f3ebf1d4e15fb0ec9d7447a593d2f85377e764
“KeitoIkeda” <k1083dy@gmail.com>
2026-04-13 22:40:41 +0900

    アイコン非表示

--- resources/js/Pages/AdminDashboard.jsx
+++ resources/js/Pages/AdminDashboard.jsx
@@ -472,7 +472,7 @@ export default function AdminDashboard({ auth }) {
             <Modal show={isRejectConfirmModalOpen} onClose={() => setIsRejectConfirmModalOpen(false)} maxWidth="sm">
                 <div className="p-6">
                     <h2 className="text-lg font-bold text-gray-900 flex items-center">
-                        <svg className="w-6 h-6 text-red-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path></svg>
+                        {/* <svg className="w-6 h-6 text-red-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path></svg> */}
                         申請の却下
                     </h2>
                     
